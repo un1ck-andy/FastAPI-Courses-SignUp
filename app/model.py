81b84fbc7dcd0ec757228f3ddb4a4fb1fa3d3d0f
un1ck-andy <105160048+un1ck-andy@users.noreply.github.com>
2022-08-13 14:24:09 +0200

    testing pre commit hooks

--- app/model.py
+++ app/model.py
@@ -1,48 +1,54 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
 
 class CourseSchema(BaseModel):
-    id: int = Field(default=None)
+    course_id: int = Field(default=None)
     title: str = Field(...)
     description: str = Field(...)
-    student_id: list = Field(...)
-    
+
     class Config:
+        orm_mode = True
         schema_extra = {
             "example": {
                 "title": "Interesting course title",
-                "description": "Not so boring description"
+                "description": "Not so boring description",
             }
         }
 
 
 class StudentSchema(BaseModel):
-    id: int = Field(default=None)
+    user_id: int = Field(default=None)
     fullname: str = Field(...)
     email: EmailStr = Field(...)
     password: str = Field(...)
 
     class Config:
+        orm_mode = True
         schema_extra = {
             "example": {
                 "fullname": "Thomas Anderson",
                 "email": "neo@matrix.has.you",
-                "password": "weakpassword"
+                "password": "weakpassword",
             }
         }
 
+
 class StudentLoginSchema(BaseModel):
-    email: EmailStr = Field(...)
-    password: str = Field(...)
+    email: EmailStr = Field(default=None)
+    password: str = Field(default=None)
 
     class Config:
         schema_extra = {
             "example": {
                 "email": "neo@matrix.has.you",
-                "password": "weakpassword"
+                "password": "weakpassword",
             }
         }
 
-class ApplicationsListChema(BaseModel):
+
+class CourseSignUp(BaseModel):
     id: int = Field(default=None)
     course_id: int = Field(default=None)
     student_id: int = Field(default=None)
--- main.py
+++ main.py
@@ -1,121 +1,203 @@
-import uvicorn
-from fastapi import FastAPI, Body, Depends
+from fastapi import Body
+from fastapi import Depends
+from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
+from sqlalchemy import Column
+from sqlalchemy import create_engine
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
 
-from app.model import CourseSchema, StudentSchema, StudentLoginSchema, ApplicationsListChema
 from app.auth.auth_bearer import JWTBearer
 from app.auth.auth_handler import signJWT
+from app.model import CourseSchema
+from app.model import StudentLoginSchema
+from app.model import StudentSchema
+
+base = declarative_base()
 
 
-import sqlite3
+class Student(base):
+    __tablename__ = "students"
+    user_id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        unique=True,
+        autoincrement=True,
+    )
+    fullname = Column(String)
+    email = Column(String)
+    password = Column(String)
 
-# courses = [
-#     {
-#         "id": 1,
-#         "title": "First course",
-#         "description": "Interesting course",
-#         "student_id": ""
-#     },
-#     {
-#         "id": 2,
-#         "title": "Second course",
-#         "description": "Not so boring as the next one",
-#         "student_id": ""
-#     },
-#     {
-#         "id": 3,
-#         "title": "Third course",
-#         "description": "Don't listen to all, this course is awesome",
-#         "student_id": ""
-#     },
-# ]
 
-# students = [
-#     {
-#         "id": 1,
-#         "fullname": "Thomas Anderson",
-#         "email": "neo@matrix.has.you",
-#         "password": "weakpassword"
-#     }
-# ]
+class Course(base):
+    __tablename__ = "courses"
+    course_id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        unique=True,
+        autoincrement=True,
+    )
+    title = Column(String)
+    description = Column(String)
 
-# course_applications = []
+
+class CourseSignUp(base):
+    __tablename__ = "course_sign_up"
+    course_sing_up_id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        unique=True,
+        autoincrement=True,
+    )
+    student_id = Column(Integer, ForeignKey("students.user_id"))
+    course_id = Column(Integer, ForeignKey("courses.course_id"))
+    student = relationship(
+        "Student", backref="signup_student", lazy="subquery"
+    )
+    course = relationship("Course", backref="signup_course", lazy="subquery")
+
+
+engine = create_engine("sqlite:///fastapi.db", echo=True)
+SessionLocal = sessionmaker(bind=engine)
+db = SessionLocal()
+
+base.metadata.create_all(engine)
 
 app = FastAPI()
 
 
-
 def check_student(data: StudentLoginSchema):
-    for student in students:
-        if student.email == data.email and student.password == data.password:
-            return True
+    check_result = (
+        db.query(Student)
+        .filter(Student.email == data.email, Student.password == data.password)
+        .first()
+    )
+    if check_result:
+        return True
     return False
 
 
 # route handlers
 
-# testing
-@app.get("/", tags=["test"])
-def greet():
-    return {"hello": "world!"}
-
-
 # Get courses
-@app.get("/courses", tags=["courses"])
-def get_courses():
-    return { "data": courses }
+@app.get(
+    "/api/v1/courses",
+    response_model=list[CourseSchema],
+    status_code=status.HTTP_200_OK,
+    tags=["Courses"],
+)
+async def get_all_courses():
+    """A list of all courses"""
+
+    courses = db.query(Course).all()
+    return courses
 
 
-@app.get("/courses/{id}", tags=["courses"])
-def get_single_course(id: int):
-    if id > len(courses):
-        return {
-            "error": "No such course with the supplied ID."
-        }
+@app.get(
+    "/api/v1/courses/{id}",
+    response_model=CourseSchema,
+    status_code=status.HTTP_200_OK,
+    tags=["Courses"],
+)
+async def get_single_course(id: int):
+    """Find a course by ID"""
+    course = db.query(Course).filter(Course.course_id == id).first()
 
-    for course in courses:
-        if course["id"] == id:
-            return {
-                "data": course
-            }
-
-
-@app.post("/courses", dependencies=[Depends(JWTBearer())], tags=["courses"])
-def add_course(course: CourseSchema):
-    course.id = len(courses) + 1
-    for c in courses:
-        if c["title"] == course.title:
-            return {
-                "data": f"Error: course named '{course.title}' already exists."
-            }
-    courses.append(course.dict())
-    return {
-        "data": course.title + " added."
-    }
+    if course is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Course not found"
+        )
+    return course
 
 
-@app.post("/students/signup", tags=["students"])
-def signup_student(student: StudentSchema = Body(...)):
-    students.append(student) # replace with db call, making sure to hash the password first
+@app.post(
+    "/api/v1/courses",
+    dependencies=[Depends(JWTBearer())],
+    status_code=status.HTTP_201_CREATED,
+    tags=["courses"],
+)
+async def add_course(course: CourseSchema):
+    """Add a new course"""
+    db_course = db.query(Course).filter(Course.title == course.title).first()
+
+    if db_course is not None:
+        raise HTTPException(
+            status_code=400, detail="Course with the same title already exists"
+        )
+
+    new_course = Course(
+        title=course.title,
+        description=course.description,
+    )
+
+    db.add(new_course)
+    db.commit()
+    return new_course
+
+
+@app.get(
+    "/api/v1/students",
+    response_model=list[StudentSchema],
+    status_code=status.HTTP_200_OK,
+    tags=["test"],
+)
+async def fetch_students():
+    """Show all students list"""
+    students = db.query(Student).all()
+    return students
+
+
+@app.post(
+    "/api/v1/students/signup",
+    status_code=status.HTTP_201_CREATED,
+    tags=["students"],
+)
+async def signup_student(student: StudentSchema = Body(...)):
+    """Add a new user"""
+    db_student = (
+        db.query(Student).filter(Student.email == student.email).first()
+    )
+
+    if db_student is not None:
+        raise HTTPException(
+            status_code=400, detail="User with the same email already exists"
+        )
+
+    new_student = Student(
+        fullname=student.fullname,
+        email=student.email,
+        password=student.password,
+    )
+
+    db.add(new_student)
+    db.commit()
     return signJWT(student.email)
 
 
-@app.post("/students/login", tags=["students"])
-def student_login(student: StudentLoginSchema = Body(...)):
+@app.post(
+    "/api/v1/students/login",
+    status_code=status.HTTP_200_OK,
+    tags=["students"],
+)
+async def student_login(student: StudentLoginSchema = Body(default=None)):
+    """Login user"""
     if check_student(student):
         return signJWT(student.email)
-    return {
-        "error": "Wrong login details!"
-    }
-
-@app.post("/courses/signup", tags=["courses"])
-def signup_to_course(list: ApplicationsListChema):
-    for course in courses:
-        if course["id"] == list.course_id:
-            course["student_id"] += "," +str(list.student_id)
-            return {
-                "data": f"{list.student_id} was signed to the course {course['title']}"
-            }
-  
+    else:
+        raise HTTPException(status_code=401, detail="Invalid login details!")
 
 
-# id student_id course_id
+# @app.post("/courses/signup", tags=["courses"])
+# def signup_to_course(list: ApplicationsListChema):
+#     for course in courses:
+#         if course["id"] == list.course_id:
+#             course["student_id"] += "," + str(list.student_id)
+#             return {"data": f"{list.student_id} was signed to the course {course['title']}"}
